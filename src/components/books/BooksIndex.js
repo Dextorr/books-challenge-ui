1e2de4e51ad1b3e269a4c8ed9943cd312c98cd2b
Dexter De Leon <dexdeleon@gmail.com>
2019-07-11 13:22:03 +0100

    axios installed and used to get books from API, storing them in state of BooksIndex.

--- src/components/books/BooksIndex.js
+++ src/components/books/BooksIndex.js
@@ -1,6 +1,12 @@
 import React from 'react'
+import axios from 'axios'
 
 class BooksIndex extends React.Component{
+
+  componentDidMount(){
+    axios.get('http://booksontap.azurewebsites.net/api/Books')
+      .then(res => this.setState({ books: res.data.results }))
+  }
 
   render(){
     return(
