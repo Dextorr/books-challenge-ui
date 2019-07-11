1b353d6130695fa4c14a77e1c9f285b4b1c6fddb
Dexter De Leon <dexdeleon@gmail.com>
2019-07-11 13:28:46 +0100

    BooksIndex component showing book titles.

--- src/components/books/BooksIndex.js
+++ src/components/books/BooksIndex.js
@@ -1,16 +1,29 @@
 import React from 'react'
 import axios from 'axios'
 
+import { Container, Row, Col } from 'react-bootstrap'
+
 class BooksIndex extends React.Component{
 
   componentDidMount(){
     axios.get('http://booksontap.azurewebsites.net/api/Books')
       .then(res => this.setState({ books: res.data.results }))
+      .catch(err => console.error(err))
   }
 
   render(){
+    if(!this.state) return null
     return(
-      <h1>Books</h1>
+      <Container>
+        <h1>Books</h1>
+        <Row>
+          {this.state.books.map(book =>
+            <Col key={book.id}>
+              <h3>{book.title}</h3>
+            </Col>
+          )}
+        </Row>
+      </Container>
     )
   }
 }
